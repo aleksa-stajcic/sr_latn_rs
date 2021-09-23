0924c493fad686df76c4b2ec0bd970855ee8163f
Aleksa Stajcic <49615937+aleksa-stajcic@users.noreply.github.com>
2021-09-23 17:25:07 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -7,4 +7,4 @@ declare(strict_types=1);
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'communityengineering_sr_latn_rs', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'aleksa-stajcic_sr_latn_rs', __DIR__);
